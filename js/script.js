ea974d4d8a260b36af20607ecbcca16a788e7bc1
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-02-05 23:22:52 +0600

    callback function, map, filter, find, reduce

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ let percent=(price,percent)=>{
 let arrayOne=[1,2,3,4,5];
 let arrayTwo=[6,7,8,9,10];
 
-console.log(...arrayOne,...arrayTwo);
+// console.log(...arrayOne,...arrayTwo);
 
 //Destructuring
 
@@ -32,3 +32,36 @@ let {name,age}={
     age:22
 }
 // console.log(name)
+//array----> for of
+//object----> for in
+//map 
+
+let num=[4,5,6,7,12];
+let double=[];
+
+for(let number of num){
+    let output=number*2;
+    // console.log(output);
+    
+}
+
+//callback function 
+//A callback is a function passed as an argument to another function This technique allows a function to call another function A callback function can run after another function has finished 
+
+//map,filter, find ,reduce
+
+// function min(nums) { return Math.min(nums) }
+// console.log(min( [1,3,2 ]));
+
+// const cube=x=> x*x*x; 
+// console.log(cube(2))
+// const [a, b] = [1,2,3,4,45,5]; 
+// console.log(a+b);
+
+// const nums = [1,2,3,4,5];
+// let output = nums.filter(n => n%2);
+// console.log(output);
+
+const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
+const {price}=product;
+console.log(price)
